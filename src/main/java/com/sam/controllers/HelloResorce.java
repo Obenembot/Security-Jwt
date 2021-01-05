f056c8a5f21f7c539c69bff16d195c1e7d2142f3
Obenembot <embottabi@gmail.com>
2021-01-05 23:47:49 +0200

    jwt security

--- src/main/java/com/sam/controllers/HelloResorce.java
+++ src/main/java/com/sam/controllers/HelloResorce.java
@@ -1,13 +1,55 @@
 package com.sam.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sam.models.AuthenticationRequest;
+import com.sam.models.AuthenticationResponse;
+import com.sam.services.MyUserDetailsService;
+import com.sam.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
+
 
 @RestController
 public class HelloResorce {
 
+    private Logger logger = Logger.getGlobal();
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            logger.info("Authentication Body { } "+authenticationRequest.toString());
+            this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()));
+
+
+
+        } catch (BadCredentialsException b) {
+            logger.info("Incorrect Username or Password  { } ");
+            throw new Exception("Incorrect Username or Password " + b);
+        }
+
+        final UserDetails userDetails = this.userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwtToken = this.jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwtToken));
     }
 }
--- src/main/java/com/sam/services/MyUserDetailsService.java
+++ src/main/java/com/sam/services/MyUserDetailsService.java
@@ -8,15 +8,29 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    private List<User> users = Arrays.asList(
+            new User("foo", "foo", new ArrayList<>()),
+            new User("me", "me", new ArrayList<>()),
+            new User("you", "you", new ArrayList<>()),
+            new User("run", "run", new ArrayList<>())
+    );
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+        User userr = null;
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                userr = user;
+            }
+        }
 
-      return   new User("foo","foo", new ArrayList<>());
+        return userr;
     }
 }
